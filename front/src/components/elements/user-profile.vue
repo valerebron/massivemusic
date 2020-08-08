4fb4469429f9736ec72aa338c1fba401f339b017
ricardo <dev@valerebron.com>
2020-08-08 23:16:30 +0200

    FEAT: users v2

---
+++ front/src/components/elements/user-profile.vue
@@ -0,0 +1,94 @@
+<template>
+  <main class="user-profile page--container">
+    <div>
+      <figure class="user-list">
+        <img :src="'/avatars/'+user.id+'-300px.png'" />
+        <figcaption class="user-list__captions">
+          <h1>{{ user.name }}</h1>
+          <h2 v-if="user.role !== 'ROBOT'">{{ user.email }}</h2>
+          <h3>
+            <router-link :to="'/user/'+$route.params.user_id+'/tracks'">
+              <b>{{ user.tracks.length }}</b> track(s) added
+            </router-link>
+          </h3>
+          <h4>subscribed {{ Date.parse(user.createdAt) | moment('from', 'now') }}</h4>
+          <h5>last login {{ Date.parse(user.updatedAt) | moment('from', 'now') }}</h5>
+        </figcaption>
+      </figure>
+    </div>
+  </main>
+</template>
+
+<script>
+  import gql from 'graphql-tag'
+  export default {
+    name: 'user-profile',
+    data: function() {
+      return {
+        user: {
+          tracks: [],
+          id: this.$route.params.user_id,
+        },
+      }
+    },
+    methods: {
+      fetchUser: function() {
+        if(this.$route.params.user_id === 'me') {
+          this.user = this.$store.getters.session.user
+        }
+        else {
+          window.apollo.query({
+            variables: {
+              user_id: parseInt(this.$route.params.user_id),
+            },
+            query: gql`
+              query(
+                  $user_id: Int,
+              ) {
+                user(
+                  user_id: $user_id,
+                ) {
+                  id
+                  email
+                  name
+                  role
+                  createdAt
+                  updatedAt
+                  tracks {
+                    yt_id
+                    title
+                    artist
+                    style {
+                      id
+                    }
+                  }
+                }
+              }
+            `,
+          }).then((res) => {
+          this.user = res.data.user
+          }).catch(() => {
+            console.log('%c●', 'color: red', 'user error')
+          })
+        }
+      },
+    },
+    mounted: function() {
+      this.fetchUser()
+    },
+    updated: function() {
+      this.fetchUser()
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .user-list {
+    display: flex;
+    flex-direction: row;
+    padding-top: 10vh;
+    &__captions {
+      padding-left: 12px;
+    }
+  }
+</style>
---
+++ front/src/store/users.js
@@ -0,0 +1,7 @@
+const getters = {
+  user_tracks: state => state.tracks,
+}
+
+export default {
+  getters,
+}
