fd9d7ebb2127fc73382708c4e13f384a54417d3f
ricardo <dev@valerebron.com>
2020-11-17 00:21:47 +0100

    FEAT: admin dont go to pending by default

--- front/src/components/pages/admin.vue
+++ front/src/components/pages/admin.vue
@@ -81,9 +81,6 @@
       if(!this.$store.getters.isAdmin) {
         this.$router.push('home')
       }
-      else {
-        document.querySelector('.pending.checkbox input').click()
-      }
     },
   }
 </script>
